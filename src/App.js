959a1e5386e4fb683980860b0e08bb377a9d085a
Lachlan Heywood <lachlan@heywood.net.au>
2020-11-11 16:04:20 -0500

    add api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+import { Button, Container, Grid, TextField, Typography } from '@material-ui/core';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import MovieCard from './components/MovieCard';
+import { setData } from './redux/actions';
 
 function App() {
+  const [search, setSearch] = useState('');
+  const dispatch = useDispatch();
+  const movies = useSelector((state) => state.results)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const encodedSearch = encodeURIComponent(search);
+    // get movies from API
+    fetch(`http://www.omdbapi.com/?apikey=59354c85&s=${encodedSearch}`)
+      .then(res => res.json())
+      .then(data => {
+        dispatch(setData(data.Search));
+      })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Typography variant="h1" align="center">Scene It!</Typography>
+      <Grid container direction="row" justify="center" alignItems="center">
+        <form onSubmit={handleSubmit}>
+          <TextField label="Search" variant="outlined" value={search} onChange={(e) => {setSearch(e.target.value)}} />
+          <Button type="submit" variant="contained" size="large">Search</Button>
+        </form>
+      </Grid>
+      <Grid container spacing={3}>
+        { movies.map(movie => {
+          return (
+            <Grid item xs={3} key={movie.imdbID}>
+              <MovieCard movie={movie} />
+            </Grid>
+          )
+        }) }
+      </Grid>
+    </Container>
   );
 }
 
